943c104156d936413bd69bec4b48118d5759fd91
Lightfooted <vesselofbalance@hotmail.com>
2021-09-07 21:33:11 -0400

    data for tables

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES 
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2), 
+('Software Engineer', 120000, 2),
+('Accountant', 125000, 3), 
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('John', 'Doe', 1),
+('Mike', 'Chan', 1),
+('Ashley', 'Rodriquez', 2, NULL),
+('Kevin', 'Tupik', 2),
+('Malia', 'Brown', 3, NULL),
+('Sarah', 'Lourd', 3, NULL),
+('Tom', 'Allen', 4),
+('Tammer', 'Galal', 2);
